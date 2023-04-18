cd81d3bb0c76ca137d60c69e97cc77c31dbd718e
CristianPessatti <32284997+CristianPessatti@users.noreply.github.com>
2023-04-17 23:44:48 -0300

    Delete utilsEnigma.py

--- utilsEnigma.py
+++
@@ -1,7 +0,0 @@
-def find(item, lista):
-    for i in range(len(lista)):
-        if(lista[i] == item):
-            return i
-
-def swap(pos1, pos2, lista):
-    lista[pos1], lista[pos2] = lista[pos2], lista[pos1]
